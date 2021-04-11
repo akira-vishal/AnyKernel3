b8ada1f86f162474f32d299131afc3bb5783814b
akira-vishal <vishal.rockstar7011@gmail.com>
2021-04-12 06:03:32 +0700

    remove magisk/recovery

--- tools/magisk/nexus/service.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-MODDIR=${0%/*}
-INFO=/data/adb/modules/.nexus-files
-MODID=nexus
-LIBDIR=/system
-MODPATH=/data/adb/modules/nexus
-sleep 30
-$MODDIR/system/bin/perfsave 2>/dev/null
-
